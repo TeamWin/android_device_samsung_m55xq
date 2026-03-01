3b0cbd2a2d3c6bde216eb2c1db039fd15f148bcd
subhu2008 <manoramanayak755@gmail.com>
2026-03-01 21:48:37 +0530

    Little correction in cmdline from onsole=null to console=null

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -61,7 +61,7 @@ BOARD_KERNEL_CMDLINE :=  \
 	video=vfb:640x400,bpp=32,memsize=3072000 \
 	printk.devkmsg=on \
 	firmware_class.path=/vendor/firmware_mnt/image \
-	onsole=null \
+	console=null \
 	bootconfig \
 	androidboot.hardware=qcom \
 	hardware=qcom \
